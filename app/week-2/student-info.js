a77ccedcdfab2feec87212df8943e5b820048501
Bhupinder <bhupindersaini0699@gmail.com>
2024-01-17 00:39:25 -0700

    initial

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <div>Bhupinder Singh</div>
+      <Link href="https://github.com/">Github</Link>
+    </div>
+  );
+}
---
+++ components/Navigation.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Navigation(){
+    return(
+        <div>
+            <Link href="/week-2" className="text-xl font-semibold">Week 2 Assignment</Link>
+        </div>
+    )
+}
