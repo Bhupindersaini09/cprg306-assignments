6e1e5e636acabf52d2fc90e5df82783814eac695
Bhupinder <bhupindersaini0699@gmail.com>
2024-02-05 22:27:42 -0700

    week-3

---
+++ app/week-3/page.js
@@ -0,0 +1,11 @@
+import ItemList from "./item-list"
+
+export default function Page(){
+    return(
+        <main>
+            <h1 className="text-4xl p-5 font-bold">SHOPPING LIST</h1>
+            <ItemList></ItemList>
+
+        </main>
+    )
+}
