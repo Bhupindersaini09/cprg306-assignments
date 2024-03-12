c8b2f62e7f654be9b1c65fb4e9d22e2a43a775b7
Bhupinder <bhupindersaini0699@gmail.com>
2024-03-11 21:51:33 -0600

    week-7

--- app/week-6/item.js
+++ app/week-6/item.js
@@ -7,7 +7,7 @@ export default function Item({ name, quantity, category }) {
         <ul className="p-2.5 ml-5 pt2 border-2 border-zinc-950 mb-3 w-96  bg-zinc-800">
           <li className="font-bold text-xl">{name}</li>
           <li className="">
-            But {quantity} in {category}
+            Buy {quantity} in {category}
           </li>
         </ul>
       </div>
